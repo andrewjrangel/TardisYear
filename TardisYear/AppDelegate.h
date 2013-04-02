24206cc9eaf80ef881348251766182eb519e47e3
Andrew Rangel <andrewjrangel@gmail.com>
2013-04-01 23:18:53 -0500

    Initial Commit

---
+++ TardisYear/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TardisYear
+//
+//  Created by Ventura Rangel on 4/1/13.
+//  Copyright (c) 2013 Andrews Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
